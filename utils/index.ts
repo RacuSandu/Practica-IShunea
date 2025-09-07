9e1314e330c2483c4cb70de03cd7ffd7a8966e9c
RacuSandu <sandu.racu2004@gmail.com>
2025-09-07 21:37:25 +0300

    Server-side rendering, custom filters and show more

--- utils/index.ts
+++ utils/index.ts
@@ -1,11 +1,13 @@
-import { CarProps } from "@/types";
+import { CarProps, FilterProps } from "@/types";
 
-export async function fetchCars() {
-    const headers = {
+export async function fetchCars(filters: FilterProps) {
+  const {manufacturer, year, fuel, limit, model} = filters;
+
+  const headers = {
         'X-RapidAPI-Key': 'ed941fa1f7mshdd773357ace436cp173abdjsn19f3017a0ec5',
         'X-RapidAPI-Host': 'cars-by-api-ninjas.p.rapidapi.com'
  }
- const response = await fetch('https://cars-by-api-ninjas.p.rapidapi.com/v1/cars?model=q3', {
+ const response = await fetch(`https://cars-by-api-ninjas.p.rapidapi.com/v1/cars?make=${manufacturer}&year=${year}&model=${model}&limit=${limit}&fuel_type=${fuel}`, {
     headers: headers,
  });
  const result = await response.json();
@@ -38,4 +40,14 @@ export const generateCarImageUrl = (car: CarProps, angle?: string) => {
   url.searchParams.append('angle', `${angle}`);
 
   return `${url}`;
+}
+
+export const updateSearchParams = (type: string, value: string) => {
+  const searchParams = new URLSearchParams(window.location.search);
+
+  searchParams.set(type, value)
+
+  const newPathname = `${window.location.pathname}?${searchParams.toString()}`
+  
+  return newPathname;
 }
